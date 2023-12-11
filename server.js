169152e517234dc01cb57d391ad93c50b788829b
Nandaram Prashanth kumar <kmrprashanth92@gmail.com>
2023-12-11 14:58:19 -0500

    stripe payment added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const multer = require('multer');
+const stripe = require('stripe')('sk_test_51OKVqaHWdYWtkALOq6YxogC72p3SW6Y6u0QMotc7CF4sRVvKCj9oEWMxiIuSumRYsOWkos3UwZh077x23rHpadXP00ju1hHiyT');
 
 const app = express();
 const PORT = 3000;
@@ -17,42 +17,62 @@ mongoose.connect('mongodb://localhost:27017/craftconnect', {
 const User = mongoose.model('User', {
   username: String,
   password: String,
-  email: String,  
-  dob: Date,     // Add email field
-  location: String,    // Add location field
-  bio: String,         // Add bio field  // Keep the gallery field as it is
+  email: String,
+  dob: Date,
+  location: String,
+  bio: String,
 });
-// const upload = multer();
-// Signup endpoint
-app.post('/signup',async (req, res) => {
+
+app.post('/signup', async (req, res) => {
   try {
-    const { username, password, email,dob, location, bio } = req.body;
-    // const profileImage = {
-    //   data: req.file.buffer, // Image data
-    //   contentType: req.file.mimetype, // Image content type
-    // };
-    const user = new User({ username, password, email,dob, location, bio  });
+    const { username, password, email, dob, location, bio } = req.body;
+    const user = new User({ username, password, email, dob, location, bio });
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
+    console.error('Error registering user:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Login endpoint
 app.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username, password });
-    console.log('User data:', user); 
     if (user) {
-      res.status(200).json({ message: 'Login successful',user });
+      res.status(200).json({ message: 'Login successful', user });
     } else {
       res.status(401).json({ error: 'Invalid credentials' });
     }
   } catch (error) {
+    console.error('Error logging in:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
+});
+
+app.post('/payment-sheet', async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2023-10-16'}
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'eur',
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    publishableKey: 'pk_test_51OKVqaHWdYWtkALO6ghTEGviBJWi25YqqKHSvgphCk7TDhRTm1kO8WM19yoLzXzOi2acpuRBfqLcOHVBePA0uf3d0024wSYJDl'
+  });
 });
 
 app.listen(PORT, () => {
